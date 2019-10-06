63c7f6e7949d853caf7664a3d7a1276e3e14a9e0
SaltyKawaiiNeko <software@skneko.moe>
2019-10-06 12:33:18 +0200

    refactor: Move config code to its own module (#7)
    * Move config code to its own module
    
    * Remove accessor methods of Config

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use dirs::home_dir;
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub server: String,
+    pub access_token: String,
+}
+
+impl Config {
+    pub fn parse_from_disk() -> Config {
+        let config_path = home_dir()
+            .expect("Could not find home dir")
+            .join(".config/gitlab.toml");
+        let config_string = fs::read_to_string(&config_path)
+            .expect(format!("Something went wrong reading the file {:?}", &config_path).as_str());
+
+        toml::from_str(&config_string).expect("Could not parse the config")
+    }
+}
