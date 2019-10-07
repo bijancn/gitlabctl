76f31110fdbd4f1fae4d59466e6843a704665fa0
Chetan Bhasin <chetanbhasin7@gmail.com>
2019-10-07 11:10:19 +0200

    Implement Tokio For IO Parallel Tasks (#8)

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use chrono::{DateTime, Utc};
 use chrono_humanize::HumanTime;
 use clap::{App, Arg, SubCommand};
 use colored::*;
+use futures_util::future::*;
 
 use gitlab::*;
-use rayon::prelude::*;
 use std::time::Instant;
 
 const EMPTY_PARAMS: &[(&str, &str)] = &[];
@@ -12,13 +12,13 @@ const EMPTY_PARAMS: &[(&str, &str)] = &[];
 mod config;
 use config::Config;
 
-fn get_projects_for_namespace(gitlab: &Gitlab, namespace: &str) -> Vec<(String, ProjectId)> {
+async fn get_projects_for_namespace(gitlab: &Gitlab, namespace: &str) -> Vec<(String, ProjectId)> {
     let before = Instant::now();
     // There is no way to filter projects by namespace in the query parameters in v4
     let result = gitlab
         .projects(EMPTY_PARAMS)
         .unwrap_or_default()
-        .par_iter()
+        .iter()
         .filter(|p| {
             namespace.is_empty() || p.namespace.name.to_uppercase() == namespace.to_uppercase()
         })
@@ -32,7 +32,7 @@ fn get_projects_for_namespace(gitlab: &Gitlab, namespace: &str) -> Vec<(String, 
     result
 }
 
-fn get_environments_of_project(
+async fn get_environments_of_project(
     gitlab: &Gitlab,
     project_name_and_id: &(String, ProjectId),
 ) -> Vec<(String, ProjectId, Environment)> {
@@ -41,35 +41,36 @@ fn get_environments_of_project(
     gitlab
         .environments(*id, EMPTY_PARAMS)
         .unwrap_or_default()
-        .par_iter()
+        .iter()
         .map(move |e: &Environment| (name.to_owned(), id.to_owned(), e.to_owned()))
         .collect()
 }
 
-fn get_all_environments(
+async fn get_all_environments(
     gitlab: &Gitlab,
     project_names: Vec<(String, ProjectId)>,
 ) -> Vec<Vec<(String, ProjectId, Environment)>> {
     let before = Instant::now();
-    let result: Vec<Vec<(String, ProjectId, Environment)>> = project_names
+    let results = project_names
         .iter()
-        .map::<Vec<(String, ProjectId, Environment)>, _>(|x| {
-            get_environments_of_project(&gitlab, x)
+        .map(|name| get_environments_of_project(&gitlab, name));
+
+    join_all(results)
+        .inspect(|e| {
+            println!(
+                "Retrieved {:} environments      [{:.2?}]",
+                e.iter().map(|x| x.len()).sum::<usize>(),
+                before.elapsed()
+            )
         })
-        .collect();
-    let environments: usize = result.iter().map(|x| x.len()).sum();
-    println!(
-        "Retrieved {:} environments      [{:.2?}]",
-        environments,
-        before.elapsed()
-    );
-    result
+        .await
 }
 
-fn get_environment_details(
+
+async fn get_environment_details(
     gitlab: &Gitlab,
     all_envs: Vec<Vec<(String, ProjectId, Environment)>>,
-) -> Vec<(String, (String, String, String, String, String))> {
+) -> Result<Vec<(String, (String, String, String, String, String))>> {
     let before = Instant::now();
     let result: Vec<(String, (String, String, String, String, String))> = all_envs
         .iter()
@@ -78,9 +79,8 @@ fn get_environment_details(
                 .iter()
                 .map(
                     |(project_name, project_id, env): &(String, ProjectId, Environment)| {
-                        let env: Environment = gitlab
-                            .environment(project_id.to_owned(), env.id, EMPTY_PARAMS)
-                            .unwrap();
+                        let env: Environment =
+                            gitlab.environment(project_id.to_owned(), env.id, EMPTY_PARAMS).unwrap();
                         let last_deployment: Option<Deployment> = env.last_deployment;
                         let iid: String = last_deployment
                             .to_owned()
@@ -109,9 +109,7 @@ fn get_environment_details(
                         )
                     },
                 )
-                .filter(
-                    |x| !x.3.is_empty(), //&& x.5.as_ref().and_then(|x| x.status.as_ref()).is_some()
-                )
+                .filter(|x| !x.3.is_empty())
                 .map(|x| (x.0, x.1, x.2, x.3, x.4))
                 .collect();
             let mut commits: Vec<String> = (result).iter().map(|x| x.3.to_owned()).collect();
@@ -130,10 +128,11 @@ fn get_environment_details(
         })
         .collect();
     println!("Retrieved environments details [{:.2?}]", before.elapsed());
-    result
+    Ok(result)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let matches = App::new("gitlabctl")
         .version("0.1")
         .author("Bijan Chokoufe Nejad <bijan@chokoufe.com>")
@@ -157,27 +156,25 @@ fn main() {
         )
         .get_matches();
     if let Some(matches) = matches.subcommand_matches("get") {
-        let namespace = matches.value_of("namespace").unwrap_or("");
+        let namespace = matches.value_of("namespace").unwrap_or_default();
         let config = Config::parse_from_disk();
-        let _ = Gitlab::new(&config.server, &config.access_token)
-            .map_err(|err| {
-                println!("err {}", err);
-                err
-            })
-            .map(|gitlab| {
-                let project_names = get_projects_for_namespace(&gitlab, namespace);
-                let all_envs = get_all_environments(&gitlab, project_names);
-                let results = get_environment_details(&gitlab, all_envs);
-                if results.is_empty() {
-                    println!("There is nothing to show");
-                    return;
-                }
-                let longest_project = results.iter().map(|x| (x.1).0.len()).max().unwrap().max(7);
-                let longest_env = results.iter().map(|x| (x.1).1.len()).max().unwrap().max(11);
-                let longest_depl = results.iter().map(|x| (x.1).2.len()).max().unwrap().max(10);
-                let longest_commit = results.iter().map(|x| (x.1).3.len()).max().unwrap().max(6);
-                let longest_updated = results.iter().map(|x| (x.1).4.len()).max().unwrap().max(7);
-                println!(
+        let gitlab = Gitlab::new(config.server, config.access_token)?;
+        let results = get_projects_for_namespace(&gitlab, namespace)
+            .then(|project_names| get_all_environments(&gitlab, project_names))
+            .then(|all_envs| get_environment_details(&gitlab, all_envs))
+            .await?;
+        // Early return if there is nothing to show
+        if results.is_empty() {
+            return Ok(println!("There is nothing to show"));
+        }
+
+        // Show results otherwise
+        let longest_project = results.iter().map(|x| (x.1).0.len()).max().unwrap().max(7);
+        let longest_env = results.iter().map(|x| (x.1).1.len()).max().unwrap().max(11);
+        let longest_depl = results.iter().map(|x| (x.1).2.len()).max().unwrap().max(10);
+        let longest_commit = results.iter().map(|x| (x.1).3.len()).max().unwrap().max(6);
+        let longest_updated = results.iter().map(|x| (x.1).4.len()).max().unwrap().max(7);
+        println!(
                     "{:longest_project$}  {:longest_env$}  {:longest_depl$}  {:longest_commit$}  {:longest_updated$}",
                     "PROJECT",
                     "ENVIRONMENT",
@@ -190,7 +187,7 @@ fn main() {
                     longest_commit = longest_commit,
                     longest_updated = longest_updated
                 );
-                results.iter().for_each(|r| {
+        Ok(results.iter().for_each(|r| {
                     println!(
                         "{:longest_project$}  {:longest_env$}  {:longest_depl$}  {:longest_commit$}  {:longest_updated$}",
                         (r.1).0.color(r.0.to_owned()),
@@ -203,10 +200,9 @@ fn main() {
                         longest_depl = longest_depl,
                         longest_commit = longest_commit,
                         longest_updated = longest_updated
-                    );
-                });
-            });
+                    )
+                }))
     } else {
-        println!("Why don't you try the get command?");
+        Ok(println!("Why don't you try the get command?"))
     }
 }
