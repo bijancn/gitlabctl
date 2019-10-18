289b5f9bcc01f5c7f6de055393a92a8a444818c6
Bijan Chokoufe Nejad <bijan.chokoufe@gmail.com>
2019-10-18 16:25:04 +0200

    Decouple fetching from building

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use chrono_humanize::HumanTime;
 use clap::{App, Arg, SubCommand};
 use colored::*;
 use futures_util::future::*;
+use itertools::Itertools;
 
 use gitlab::*;
 use std::time::Instant;
@@ -11,6 +12,15 @@ const EMPTY_PARAMS: &[(&str, &str)] = &[];
 
 mod config;
 use config::Config;
+
+#[derive(Clone)]
+pub struct EnvironmentRow {
+    pub project_name: String,
+    pub environment_name: String,
+    pub deployment_by: String,
+    pub commit_sha: String,
+    pub updated: String,
+}
 
 async fn get_projects_for_namespace(gitlab: &Gitlab, namespace: &str) -> Vec<(String, ProjectId)> {
     let before = Instant::now();
@@ -54,7 +64,6 @@ async fn get_all_environments(
     let results = project_names
         .iter()
         .map(|name| get_environments_of_project(&gitlab, name));
-
     join_all(results)
         .inspect(|e| {
             println!(
@@ -66,69 +75,67 @@ async fn get_all_environments(
         .await
 }
 
+async fn build_environment_row(
+    gitlab: &Gitlab,
+    project_name: &String,
+    project_id: &ProjectId,
+    env: &Environment,
+) -> Result<EnvironmentRow> {
+    let env: Environment = gitlab
+        .environment(project_id.to_owned(), env.id, EMPTY_PARAMS)
+        .unwrap();
+    let last_deployment: Option<Deployment> = env.last_deployment;
+    let iid: String = last_deployment
+        .to_owned()
+        .map(|deployment| {
+            let username = deployment.user.username.to_string();
+            deployment.iid.to_string() + " by " + &(username)
+        })
+        .unwrap_or_default();
+    let commit: String = last_deployment
+        .to_owned()
+        .and_then(|x: Deployment| x.deployable.commit.short_id)
+        .unwrap_or_default();
+    let now = Utc::now();
+    let updated: String = last_deployment
+        .to_owned()
+        .map(|x| DateTime::parse_from_rfc3339(&x.created_at).unwrap())
+        .map(|x| HumanTime::from(x.signed_duration_since(now)).to_string())
+        .unwrap_or_default();
+    Ok(EnvironmentRow {
+        project_name: project_name.to_owned(),
+        environment_name: env.name,
+        deployment_by: iid,
+        commit_sha: commit,
+        updated: updated,
+    })
+}
+
+fn no_duplicates(results: &Vec<EnvironmentRow>) -> bool {
+    let length = {
+        let mut commits: Vec<String> = results.iter().map(|x| x.commit_sha.clone()).collect();
+        commits.dedup();
+        commits.len()
+    };
+    !length == results.len()
+}
 
 async fn get_environment_details(
     gitlab: &Gitlab,
     all_envs: Vec<Vec<(String, ProjectId, Environment)>>,
-) -> Result<Vec<(String, (String, String, String, String, String))>> {
+) -> Result<Vec<EnvironmentRow>> {
     let before = Instant::now();
-    let result: Vec<(String, (String, String, String, String, String))> = all_envs
-        .iter()
-        .flat_map::<Vec<_>, _>(|envs_of_project| {
-            let result: Vec<(String, String, String, String, String)> = envs_of_project
-                .iter()
-                .map(
-                    |(project_name, project_id, env): &(String, ProjectId, Environment)| {
-                        let env: Environment =
-                            gitlab.environment(project_id.to_owned(), env.id, EMPTY_PARAMS).unwrap();
-                        let last_deployment: Option<Deployment> = env.last_deployment;
-                        let iid: String = last_deployment
-                            .to_owned()
-                            .map(|deployment| {
-                                let username = deployment.user.username.to_string();
-                                deployment.iid.to_string() + " by " + &(username)
-                            })
-                            .unwrap_or_default();
-                        let commit: String = last_deployment
-                            .to_owned()
-                            .and_then(|x: Deployment| x.deployable.commit.short_id)
-                            .unwrap_or_default();
-                        let now = Utc::now();
-                        let updated: String = last_deployment
-                            .to_owned()
-                            .map(|x| DateTime::parse_from_rfc3339(&x.created_at).unwrap())
-                            .map(|x| HumanTime::from(x.signed_duration_since(now)).to_string())
-                            .unwrap_or_default();
-                        (
-                            project_name.to_owned(),
-                            env.name,
-                            iid,
-                            commit,
-                            updated,
-                            last_deployment.to_owned(),
-                        )
-                    },
-                )
-                .filter(|x| !x.3.is_empty())
-                .map(|x| (x.0, x.1, x.2, x.3, x.4))
-                .collect();
-            let mut commits: Vec<String> = (result).iter().map(|x| x.3.to_owned()).collect();
-            commits.dedup();
-            if commits.len() == 1 {
-                result
-                    .iter()
-                    .map(|r| (String::from("green"), r.to_owned()))
-                    .collect()
-            } else {
-                result
-                    .iter()
-                    .map(|r| (String::from("red"), r.to_owned()))
-                    .collect()
-            }
-        })
-        .collect();
-    println!("Retrieved environments details [{:.2?}]", before.elapsed());
-    Ok(result)
+
+    join_all(all_envs.iter().flat_map::<Vec<_>, _>(|envs_of_project| {
+        envs_of_project
+            .iter()
+            .map(|x| build_environment_row(gitlab, &x.0, &x.1, &x.2))
+            .collect()
+    }))
+    .inspect(|_| println!("Retrieved environments details [{:.2?}]", before.elapsed()))
+    .await
+    .into_iter()
+    .collect()
 }
 
 #[tokio::main]
@@ -163,17 +170,46 @@ async fn main() -> Result<()> {
             .then(|project_names| get_all_environments(&gitlab, project_names))
             .then(|all_envs| get_environment_details(&gitlab, all_envs))
             .await?;
+        let results: Vec<&EnvironmentRow> = results
+            .iter()
+            .filter(|x| !x.commit_sha.is_empty())
+            .collect();
         // Early return if there is nothing to show
         if results.is_empty() {
             return Ok(println!("There is nothing to show"));
         }
 
         // Show results otherwise
-        let longest_project = results.iter().map(|x| (x.1).0.len()).max().unwrap().max(7);
-        let longest_env = results.iter().map(|x| (x.1).1.len()).max().unwrap().max(11);
-        let longest_depl = results.iter().map(|x| (x.1).2.len()).max().unwrap().max(10);
-        let longest_commit = results.iter().map(|x| (x.1).3.len()).max().unwrap().max(6);
-        let longest_updated = results.iter().map(|x| (x.1).4.len()).max().unwrap().max(7);
+        let longest_project = results
+            .iter()
+            .map(|x| x.project_name.len())
+            .max()
+            .unwrap()
+            .max(7);
+        let longest_env = results
+            .iter()
+            .map(|x| x.environment_name.len())
+            .max()
+            .unwrap()
+            .max(11);
+        let longest_depl = results
+            .iter()
+            .map(|x| x.deployment_by.len())
+            .max()
+            .unwrap()
+            .max(10);
+        let longest_commit = results
+            .iter()
+            .map(|x| x.commit_sha.len())
+            .max()
+            .unwrap()
+            .max(6);
+        let longest_updated = results
+            .iter()
+            .map(|x| x.updated.len())
+            .max()
+            .unwrap()
+            .max(7);
         println!(
                     "{:longest_project$}  {:longest_env$}  {:longest_depl$}  {:longest_commit$}  {:longest_updated$}",
                     "PROJECT",
@@ -187,21 +223,34 @@ async fn main() -> Result<()> {
                     longest_commit = longest_commit,
                     longest_updated = longest_updated
                 );
-        Ok(results.iter().for_each(|r| {
+        let groups = results
+            .into_iter()
+            .group_by(|r| r.project_name.clone())
+            .into_iter()
+            .map(|(_, group)| group.cloned().collect())
+            .collect::<Vec<Vec<EnvironmentRow>>>();
+        Ok(for group in groups {
+            let color = if no_duplicates(&group) {
+                "green"
+            } else {
+                "red"
+            };
+            group.into_iter().for_each(|r| {
                     println!(
                         "{:longest_project$}  {:longest_env$}  {:longest_depl$}  {:longest_commit$}  {:longest_updated$}",
-                        (r.1).0.color(r.0.to_owned()),
-                        (r.1).1.color(r.0.to_owned()),
-                        (r.1).2.color(r.0.to_owned()),
-                        (r.1).3.color(r.0.to_owned()),
-                        (r.1).4.color(r.0.to_owned()),
+                        r.project_name.color(color),
+                        r.environment_name.color(color),
+                        r.deployment_by.color(color),
+                        r.commit_sha.color(color),
+                        r.updated.color(color),
                         longest_project = longest_project,
                         longest_env = longest_env,
                         longest_depl = longest_depl,
                         longest_commit = longest_commit,
                         longest_updated = longest_updated
                     )
-                }))
+                })
+        })
     } else {
         Ok(println!("Why don't you try the get command?"))
     }
